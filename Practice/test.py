85b50418cb1aaae3005140fd2a61bb73381cf7ba
Andy <anjocee@gmail.com>
2022-01-19 00:15:05 -0500

    2022/01/18

--- Practice/test.py
+++ Practice/test.py
@@ -1,54 +1,11 @@
-#from bitstring import Bits
-import sys
-'''
-a=0xFFFFFFFF
-print(a)
-print(~0xFFFFFFFF)
-print(~~0xFFFFFFFF)
-print(~(a^0xFFFFFFFF))
-print(bin(~(0xF)))
-print(0xF)
-'''
-'''
-print(bin(-4))
-print(bin(2))
-print(bin(-4^2))
-print(int(bin(-4^4),0))
-print(int("-0b110",2))
-'''
+from itertools import combinations
 
-# 1.输入为十进制负数，求补码
-# 2. 输入为负数的补码，求值
-def twos(val, bytes):
-    import sys
-    #val = int(val_str, 2)
-    b = val.to_bytes(bytes, byteorder=sys.byteorder, signed=False)       
-    return int.from_bytes(b, byteorder=sys.byteorder, signed=True)
+def P1(ele):
+    return ele+1
 
+def equals1(ele):
+    return ele==1
 
-def encode(val,byte): # int->comp
-    return val.to_bytes(byte,byteorder=sys.byteorder,signed=True)
-    
-def decode(val_str,byte): # comp->int
-    val=int(val_str,2)
-    b=val.to_bytes(byte,byteorder=sys.byteorder,signed=True)
-    #print(b)
-    ret=int.from_bytes(b,byteorder=sys.byteorder,signed=True)
-    return ret
-
-def twos_comp(val, bits):
-    """compute the 2's complement of int value val"""
-    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-        val = val - (1 << bits)        # compute negative value
-    return val       
-
-
-print(twos(254,1))
-#print(decode("1001",1))
-#print(encode(10,1))
-#val_str="1001"
-#val=-9
-#valbyte=val.to_bytes(1,byteorder=sys.byteorder, signed=True)
-#print(valbyte)
-#val_str="10000001"
-#print(twos(val_str,1))
+print(map(equals1,[1,2,3,4,5]))
+print(list(map(P1,[1,2,3,4,5])))
+print(list(combinations([1,2,3,4],2)))
--- Problems/401_BinaryWatch.py
+++ Problems/401_BinaryWatch.py
@@ -46,3 +46,11 @@ class Solution:
         return hourStr+":"+minStr
                     
 
+class Solution2:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        ret=[]
+        for i in range(12):
+            for j in range(60):
+                if (bin(i)+bin(j)).count("1")==turnedOn:
+                    ret.append(str(i)+":"+str(j).rjust(2,"0"))
+        return ret
